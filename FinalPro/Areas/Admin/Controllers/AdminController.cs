8fc5799b011508745310c21f6e2c35342faacf5f
beshirazizov <bashirja@code.edu.az>
2018-07-20 18:02:02 +0400

    Add project files.

---
+++ FinalPro/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalPro.Models;
+
+namespace FinalPro.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        libraffEntities db = new libraffEntities();
+
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            if (Session["loggedadmin"] != null)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult AddAuthor()
+        {
+            if (Session["loggedadmin"] != null)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        [HttpPost]
+        public ActionResult AddAuthor(string authorN)
+        {
+            if (Session["loggedadmin"] != null)
+            {
+                if (authorN == "")
+                {
+                    ViewBag.error = "Please fill the box.";
+                    return View();
+                }
+                Author author = db.Authors.FirstOrDefault(au => au.Name == authorN);
+                if (author != null)
+                {
+                    ViewBag.error = "This author is already in database.";
+                    return View();
+                }
+
+                db.Authors.Add(new Author
+                {
+                    Name = authorN,
+                });
+                db.SaveChanges();
+                ViewBag.succ = "Added author to database successfully.";
+                return View();
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        [ActionName("Login")]
+        public ActionResult LoginGet()
+        {
+            if (Session["loggedadmin"] == null)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult LoginPost(string username, string password)
+        {
+            Moderator moderator = db.Moderators.FirstOrDefault(rd => rd.Username == username && rd.Password == password);
+            if (moderator == null)
+            {
+                ViewBag.error = "Username or password is wrong.";
+                return View();
+            }
+            Session["loggedadmin"] = moderator;
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public ActionResult AddBook()
+        {
+            if (Session["loggedadmin"] != null)
+            {
+                ViewBag.Authors = db.Authors.ToList();
+                return View();
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        [HttpPost]
+        public ActionResult AddBook(string name, string author, string description, int quantity)
+        {
+            if (Session["loggedadmin"] != null)
+            {
+                ViewBag.Authors = db.Authors.ToList();
+                if (name != "" && author != "Select Author ..." && description != "" && quantity > 0)
+                {
+                    if (!db.Books.Any(bk => bk.Name == name && bk.Author.Name == author))
+                    {
+                        db.Books.Add(new Book
+                        {
+                            Name = name,
+                            AuthorID = db.Authors.First(au => au.Name == author).ID,
+                            Descript = description,
+                            Cover = "satre.jpg",
+                            Pdf = "pablo.pdf",
+                            Quantity = quantity,
+                            Available = quantity,
+                            Status = 1,                           
+                        });
+                        db.SaveChanges();
+                        ViewBag.succ = "Added book to database successfully.";
+                        return View();
+                    }
+                    ViewBag.error = "This book is already in database.";
+                    return View();
+                }
+                ViewBag.error = "Please fill all the boxes.";
+                return View();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Delete()
+        {
+            if (Session["loggedadmin"] != null)
+            {
+                ViewBag.Books = db.Books.Where(bk => bk.Status == 1).ToList();
+                return View();
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult DeleteBook(int bkid)
+        {
+            if (Session["loggedadmin"] != null)
+            {
+                db.Books.First(bk => bk.ID == bkid).Status = 0;
+                db.SaveChanges();
+                return RedirectToAction("Delete");
+            }
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ FinalPro/Controllers/HomeController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalPro.Models;
+
+namespace FinalPro.Controllers
+{
+    public class HomeController : Controller
+    {
+        libraffEntities db = new libraffEntities();
+        public ActionResult Index(int skip = 0, int take = 9)
+        {
+            ViewBag.Pages = db.Books.Where(bk=> bk.Status == 1).Count();
+            return View(db.Books.Where(bk => bk.Status == 1).OrderBy(bk => bk.ID).Skip(skip).Take(take).ToList());
+        }
+
+        [ActionName("Login")]
+        public ActionResult LoginGet()
+        {
+            if (Session["loggeduser"] == null)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult LoginPost(string email, string password)
+        {
+            Reader reader = db.Readers.FirstOrDefault(rd => rd.Email == email && rd.Password == password);
+            if (reader == null)
+            {
+                ViewBag.error = "Email or password is wrong.";
+                return View();
+            }
+            Session["loggeduser"] = reader;
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            Session["loggeduser"] = null;
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Signup()
+        {
+            if (Session["loggeduser"] == null)
+            {
+                return View();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Signup(string email, string password, string name, string confirm)
+        {
+            if (email != "" && name != "" && password != "" && confirm != "")
+            {
+                if (password.Length >= 8)
+                {
+                    if (password == confirm)
+                    {
+                        if (!db.Readers.Any(rd => rd.Email == email))
+                        {
+                            Reader rd = new Reader
+                            {
+                                Name = name,
+                                Email = email,
+                                Password = password,
+                            };
+                            db.Readers.Add(rd);
+                            db.SaveChanges();
+                            Session["loggeduser"] = rd;
+                            return RedirectToAction("Index");
+                        }
+                        ViewBag.Error = "This email is alredy taken.";
+                        return View();
+                    }
+                    ViewBag.Error = "Passwords don't match.";
+                    return View();
+                }
+                ViewBag.Error = "Password must be at least 8 chars long.";
+                return View();
+            }
+            ViewBag.Error = "Please fill all the boxes.";
+            return View();
+        }
+
+        public ActionResult MyBooks()
+        {
+            if (Session["loggeduser"] != null)
+            {
+                Reader rd = Session["loggeduser"] as Reader;
+                return View(db.BookToReaders.Where(btr => btr.ReaderID == rd.ID).ToList());
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Addbook(int bookID)
+        {
+            if (Session["loggeduser"] != null)
+            {
+                if (db.Books.Any(bk => bk.ID == bookID))
+                {
+                    Reader rd = Session["loggeduser"] as Reader;
+                    if (!db.BookToReaders.Any(bktr => bktr.BoodID == bookID && bktr.ReaderID == rd.ID))
+                    {
+                        if (db.Books.First(bk => bk.ID == bookID).Available > 0)
+                        {
+                            if (db.BookToReaders.Where(bktr => bktr.ReaderID == rd.ID).ToList().Count <= 4)
+                            {
+                                db.Books.First(bk => bk.ID == bookID).Available--;
+                                db.SaveChanges();
+
+                                db.BookToReaders.Add(new BookToReader
+                                {
+                                    BoodID = bookID,
+                                    ReaderID = rd.ID,
+                                });
+                                db.SaveChanges();
+                                return RedirectToAction("MyBooks");
+                            }
+                            TempData["error"] = "You already have 5 books. Return a book to get new one.";
+                            return RedirectToAction("MyBooks");
+                        }
+                        return RedirectToAction("Index");
+                    }
+                    TempData["error"] = "You already have this book.";
+                    return RedirectToAction("MyBooks");
+                }
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Read(int bookID)
+        {
+            if (Session["loggeduser"] != null)
+            {
+            Reader rd = Session["loggeduser"] as Reader;
+                if (db.BookToReaders.Any(btr => btr.BoodID == bookID && btr.ReaderID == rd.ID ))
+                {
+                    return File("~/Public/pdfs/" + db.Books.First(bk => bk.ID == bookID).Pdf,"application/pdf");
+                }
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Return(int bookID)
+        {
+            if (Session["loggeduser"] != null)
+            {
+                Reader rd = Session["loggeduser"] as Reader;
+                BookToReader btr = db.BookToReaders.FirstOrDefault(bt => bt.BoodID == bookID && bt.ReaderID == rd.ID);
+                if (btr != null)
+                {
+                    btr.Book.Available++;
+                    db.SaveChanges();
+
+                    db.BookToReaders.Remove(btr);
+                    db.SaveChanges();
+
+                    return RedirectToAction("MyBooks");
+                }
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
